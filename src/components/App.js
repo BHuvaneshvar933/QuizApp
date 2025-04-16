812b88d853d137644c18e884a65bdca02a4d0c2f
BHuvaneshvar933 <bhuvaneshvar.r.c@gmail.com>
2025-04-16 22:40:17 +0530

    pages

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import { useEffect, useReducer } from "react";
 
 import Header from "./Header";
 import Main from "./Main";
-import Loader from "./Loader";
+import Loader from "./Loader"
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
